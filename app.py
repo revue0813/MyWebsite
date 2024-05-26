52ab54fd0ef02b664a74ca2ebcb00e823c3f3517
revue0813 <revue0813@gmail.com>
2024-05-25 20:31:07 -0500

    created resume and military history pages and populated respective images

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -6,13 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/skills/")
+def skills():
+    return render_template("skills.html")
+
 @app.route("/resume/")
 def resume():
     return render_template("resume.html")
 
-@app.route("/skills/")
-def skills():
-    return render_template("skills.html")
+@app.route("/download/<filename>")
+def download_file(filename):
+    return send_from_directory(directory='static/documents', path=filename)
+
+@app.route("/transcripts/")
+def transcripts():
+    return render_template("transcripts.html")
+
+@app.route("/military/")
+def military():
+    return render_template("military.html")
 
 @app.route("/links/")
 def links():
