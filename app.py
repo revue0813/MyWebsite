9140af50260fe4b47a529be6f83caebabc56db87
revue0813 <revue0813@gmail.com>
2024-05-26 03:51:31 -0500

    started links page, changes to text and homepage image

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ def skills():
 def resume():
     return render_template("resume.html")
 
-@app.route("/download/<filename>")
-def download_file(filename):
-    return send_from_directory(directory='static/documents', path=filename)
-
 @app.route("/transcripts/")
 def transcripts():
     return render_template("transcripts.html")
@@ -33,6 +29,10 @@ def links():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
+
+@app.route("/download/<filename>")
+def download_file(filename):
+    return send_from_directory(directory='static/documents', path=filename)
 
 if __name__ == "__main__": # If this app is not being called as part of a module, then:
     app.run(debug=True)
